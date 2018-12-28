8ced87612dd1eb9bb47cad817a26969bda593c10
Abed Kadry <kadrya@campus.technion.ac.il>
2018-12-28 18:02:37 +0200

    first test

--- hw2.py
+++ hw2.py
@@ -2,21 +2,26 @@ f = open('input.txt', 'r')
 
 citizens = []
 candidates = []
-supports =[]
+supports = []
 for line in f:
-    while line != 'citizens':
-        if line == 'city':
-            continue
+    if line == city:
+        line += 1
         city = line.split(",")
-        continue
-    while line != 'candidates':
-        if line == 'citizens':
-            continue
-        citizens.append([line.split(",")])
-    while line != 'support':
-        if line == 'candidates':
-            continue
-        candidates.append([line.split(",")])
+    if line == 'citizens':
+        while line+1 != 'candidates':
+            line += 1
+            citizens.append([line.split(",")])
+    if line == 'candidates':
+        while line+1 != 'support':
+            line += 1
+            candidates.append([line.split(",")])
+    if line == 'output':
+        line += 1
+        out = line.split(",")
 
+print(city)
+print(citizens)
+print(candidates)
+print(out)
 
 
