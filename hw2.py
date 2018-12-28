aeb1de8a02b5dd007dfb3d89a9550baf0c3a27ec
Abed Kadry <kadrya@campus.technion.ac.il>
2018-12-28 20:22:15 +0200

    reading works

--- hw2.py
+++ hw2.py
@@ -1,27 +1,54 @@
+
 f = open('input.txt', 'r')
 
 citizens = []
 candidates = []
 supports = []
-for line in f:
-    if line == city:
-        line += 1
-        city = line.split(",")
-    if line == 'citizens':
-        while line+1 != 'candidates':
-            line += 1
-            citizens.append([line.split(",")])
-    if line == 'candidates':
-        while line+1 != 'support':
-            line += 1
-            candidates.append([line.split(",")])
-    if line == 'output':
-        line += 1
-        out = line.split(",")
+city = []
+out = []
 
+#start of reading
+
+part = ['city', 'citizens', 'candidates', 'supports', 'output']
+
+for line in f:
+    if line == 'city\n':
+        position = part[0]
+    if line == 'citizens\n':
+        position = part[1]
+    if line == 'candidates\n':
+        position = part[2]
+    if line == 'support\n':
+        position = part[3]
+    if line == 'output\n':
+        position = part[4]
+
+    if position == 'city':
+        if line != 'city\n':
+            city = line.rstrip("\r\n").split(",")
+    if position == 'citizens':
+        if line != 'citizens\n':
+            citizens.append(line.rstrip("\r\n").split(","))
+    if position == 'candidates':
+        if line != 'candidates\n':
+            candidates.append(line.rstrip("\r\n").split(","))
+    if position == 'supports':
+        if line != 'support\n':
+            supports.append(line.rstrip("\r\n").split(","))
+    if position == 'output':
+        if line != 'output\n' and line != '\n':
+            out = line.rstrip("\r\n").split(",")
+
+#end of reading
+
+#test for lists
 print(city)
 print(citizens)
 print(candidates)
+print(supports)
 print(out)
 
+#using mtmelections
 
+
+f.close()
